b2ca2035f4477f3178e7f633274a70bacb3df5b2
Aluno_Noite <luiz.mariz.dev@gmail.com>
2025-05-28 21:31:39 -0300

    commit ong

---
+++ adotepet/src/main/java/com/adotepet/model/ong/OngModel.java
@@ -0,0 +1,52 @@
+package com.adotepet.model.ong;
+
+import org.hibernate.annotations.ManyToAny;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.ForeignKey;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+
+@Table(name = "ong")
+
+@Getter
+@Setter
+
+@NoArgsConstructor
+public class OngModel {
+    
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "nome")
+    private String nome;
+
+    @Column(name = "cnpj")
+    private String cnpj;
+
+    @Column(name = "telefone")
+    private String telefone;
+
+    @Column(name = "email")
+    private String email;
+
+    @Column(name = "instagram")
+    private String instagram;
+
+    @Column(name = "responsavel_nome")
+    private String responsavelNome;
+
+    /*@ManyToOne
+    @JoinColumn(name = "endereco ", foreignKey = @ForeignKey(name = "fk_ong_endereco"))
+    private EnderecoModel endereco; */ 
+}
